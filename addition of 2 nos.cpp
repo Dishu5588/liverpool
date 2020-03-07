2b551094a284cd6b7d780ba8cc808dacdbbbad03
Dishu5588 <61327615+Dishu5588@users.noreply.github.com>
2020-03-07 12:22:50 +0530

    Add files via upload

---
+++ addition of 2 nos.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("/n enter two nos.: ");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("/n the sum of two nos. is %d",c);
+	return 0;
+}
---
+++ area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	float a,l,b;
+	printf("/n enter the length:");
+	scanf("%f",&l);
+	printf("enter the breath:");
+	scanf("%f",&b);
+	a=l*b;
+	printf("area of rect is %f",a);
+	return 0;
+}
