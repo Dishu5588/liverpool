3eb51d48fcdeb1fad3302562c5323ba47d8aecad
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-03-12 23:34:37 +0530

    Add files via upload

---
+++ Check even or odd no. using if else statement.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\n enter any no:");
+	scanf("%d",&num);
+	if(num%2==0)
+	printf("\n no is even");
+	else
+	printf("\n no is odd");
+	return 0;
+}
---
+++ Check even or odd no. using ternary operator.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\n enter no:");
+	scanf("%d",&num);
+	(num%2==0)?printf("\n the no.is even"):printf("\n the no.is odd");
+	return 0;
+}
