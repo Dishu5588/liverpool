0b0a2f743adc33a2b4367a3baa1efbd3d46a5980
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-04-03 22:18:04 +0530

    Add files via upload

---
+++ person can vote or not.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("\n enter age:");
+	scanf("%d",&age);
+	if(age>=18)
+	printf("\n per can vote");
+	else
+	printf("\n person can not vote");
+	return 0;
+}
