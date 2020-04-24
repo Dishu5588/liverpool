24775349e4688dbe04fb8156688028dd77e8e6f3
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-04-24 19:40:44 +0530

    Add files via upload

---
+++ sum of 2 nos changing last printf.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("/n enter first no.:");
+	scanf("%d",&a);
+	printf("/n enter second no.:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("/n sum of %d,%d is %d",a,b,c);
+	return 0;
+}
