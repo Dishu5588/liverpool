d84a4e4f11ef574190f8a30d3e0c00b88d94df0c
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-05-17 20:22:44 +0530

    Add files via upload

---
+++ reverse of string 2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char a[50];
+	int i,l=0;
+	printf("\n enter the string a:");
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	l++;
+	for(i=l-1;i>=0;i--)
+	{
+		printf("\n %c",a[i]);
+	}
+	return 0;
+}
