f4bb6d609bb8f9f02fcad2cedaa4933fe461fc7e
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-07-04 19:50:07 +0530

    Add files via upload

---
+++ check whether the given number is divisible by 3 or not.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("\n enter the number:");
+	scanf("%d",&n);
+	if(n%3==0)
+	printf("\n number is divisible by 3");
+	else
+	printf("\n number is not divisible by 3");
+	return 0;
+}
