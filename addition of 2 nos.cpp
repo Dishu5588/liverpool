6712a8a2ee0b7cad5c2765df53a81b383d333578
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-07-19 20:28:58 +0530

    Add files via upload

--- addition of 2 nos.cpp
+++ addition of 2 nos.cpp
@@ -1,10 +1,12 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-	int a,b,c;
-	printf("/n enter two nos.: ");
-	scanf("%d%d",&a,&b);
-	c=a+b;
-	printf("/n the sum of two nos. is %d",c);
+	int a,b;
+	cout<<"enter two number ";
+	cin>>a>>b;
+	int sum;
+	sum=a+b;
+	cout<<" sum of a and b is "<<sum;
 	return 0;
 }
