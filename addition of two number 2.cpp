28286d18da6dc21b592e06661c912803c51e9e17
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-07-19 20:28:07 +0530

    Add files via upload

---
+++ addition of two number 2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter two number ";
+	cin>>a>>b;
+	int sum;
+	sum=a+b;
+	cout<<" sum of "<<a<< " and "<<b<< " is "<<sum;
+	return 0;
+}
