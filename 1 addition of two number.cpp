7e910a97c94702830e20ab107e2e7f6dc63ee507
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-07-20 20:04:41 +0530

    Add files via upload

---
+++ 1 addition of two number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<" enter two number ";
+	cin>>a>>b;
+	c=a+b;
+	cout<<c <<" is the result of "<<a<<" and "<<" b ";
+	return 0;
+}
---
+++ 2 addition of two number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<" enter two number ";
+	cin>>a>>b;
+	c=a+b;
+	cout<<" after adding "<<a<<" and "<<b<<" sum is "<<c;
+	return 0;
+}
