ffc4c4a5eff1a8fdfc262e760e6c6a0d29af786e
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-07-25 18:03:28 +0530

    Add files via upload

---
+++ increment operator program.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=5,y;
+	y=++x;
+	cout<<" x is "<<x;
+	cout<<"\n y is "<<y ;
+	return 0;
+}
---
+++ multiplication operator in c++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=5,y=10,z;
+	z=x++*y;
+	cout<<" x is "<<x;
+	cout<<"\n y is "<<y;
+	cout<<" \n z is "<<z;
+	return 0;
+}
