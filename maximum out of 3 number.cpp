e3557031109ad3c111058ebc960dca8e844be7c5
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-08-08 21:43:42 +0530

    Add files via upload

---
+++ maximum out of 3 number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<" enter three number";
+	cin>>a>>b>>C;
+	if(a>b&&a>c)
+	{
+		cout<<" a is greater";
+	}
+	else if(b>c)
+	cout<<" b is greater";
+	else
+	cout<<" c is greater";
+	return 0
+}
