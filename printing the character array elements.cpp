31f92388ccc40fca9c6a8943710f44d15d0a6aca
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-08-28 19:49:59 +0530

    Add files via upload

---
+++ printing the character array elements.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char a[5]={'A',66,'C',68};
+	int i;
+	for(i=0;i<5;i++)
+	{
+		cout<<" "<<a[i];
+	}
+	cout<<endl;
+	return 0;
+}
---
+++ printing the float type array elements.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a[5]={1.5,2.5,3.5,4.5};
+	int i;
+	for(i=0;i<5;i++)
+	{
+		cout<<" "<<a[i];
+	}
+	return 0;
+}
