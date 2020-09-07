4f0ea3468cb212e3ee3a1c3920c8935a949c1e51
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-09-07 22:25:45 +0530

    Add files via upload

---
+++ function overloading 2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+	return a+b;
+}
+float sum (float a,float b)
+{
+	return a+b;
+}
+int main()
+{
+    cout<<sum(25,45)<<endl;
+    cout<<sum(45.5f,1.5f)<<endl;
+    cout<<"hello how are you"<<endl;
+        cout<<"hello how are you"<<endl;
+
+    return 0;
+}
