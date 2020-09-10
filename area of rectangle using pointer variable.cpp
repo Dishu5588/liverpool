9dc8d7407a48eb258ae9b096f2229fa2a1621e17
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-09-10 23:36:04 +0530

    Add files via upload

---
+++ area of rectangle using pointer variable.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class rectangle
+{
+	public:
+	int length;
+	int breadth;
+	int area()
+	{
+		return length*breadth;
+	}
+	int perimeter()
+	{
+		return 2*(length+breadth);
+	}
+};
+int main()
+{
+	rectangle r;
+	rectangle *p;
+	p=&r;
+	p->length=10;
+	p->breadth=5;
+	cout<<" area of rectangle is "<<r.area()<<endl;
+	return 0;
+}
