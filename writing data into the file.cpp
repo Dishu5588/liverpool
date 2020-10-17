b3898308acb963d49b757d21786e4c1c64f1b2a7
Deshant kumar <61327615+Dishu5588@users.noreply.github.com>
2020-10-17 20:59:31 +0530

    Add files via upload

---
+++ writing data into the file.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ofstream ofs ("my.txt",ios::trunc);
+	ofs<<"john"<<endl;
+	ofs<<25<<endl;
+	ofs<<"cs"<<endl;
+	ofs.close();
+	return 0;
+}
